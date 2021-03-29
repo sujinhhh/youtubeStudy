9c853a43ab4b3064ee692ce3d29bb09a66cb8065
Sujin <ccclalasj@gmail.com>
2021-03-29 18:22:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.info .metadata .moreBnt');
+const title =  document.querySelector(.info .metadata .title)
+
+moreBtn.addEventListener('click',() => {
+    moreBtn.classList.toggle('clicked')
+    title.classList.toggle('clamp')
+})
